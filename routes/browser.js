fe69c2f996055f31c8b330f0cc009a06dbd62532
mikioosaki <mikio.osaki@posteo.de>
2019-02-13 21:36:19 +0100

    remove template features

--- routes/browser.js
+++ routes/browser.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('browser');
+  res.render('browser',  { title: 'Browser' });
 });
 
 module.exports = router;

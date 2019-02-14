9aaaf6c272b19db57278d6496b788c68ba744809
mikioosaki <mikio.osaki@posteo.de>
2019-02-14 20:14:54 +0100

    add shortcut table

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var browserRouter = require('./routes/browser');
 var mausklickRouter = require('./routes/mausklick');
+var shortcutRouter = require('./routes/shortcuts');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/browser', browserRouter);
 app.use('/mausklick', mausklickRouter);
+app.use('/shortcuts', shortcutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/shortcuts.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('shortcuts',  { title: 'Shortcuts' });
+});
+
+module.exports = router;

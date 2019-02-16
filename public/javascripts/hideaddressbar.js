69f448478bfe65333a4b23b1c967cf50f80fe26b
mikioosaki <mikio.osaki@posteo.de>
2019-02-16 14:48:20 +0100

    format/beautify code

--- public/javascripts/hideaddressbar.js
+++ public/javascripts/hideaddressbar.js
@@ -1,12 +1,10 @@
-window.addEventListener("load", function(){  
-    if(document.height <= window.outerHeight)
-    {
+window.addEventListener("load", function () {
+    if (document.height <= window.outerHeight) {
         document.body.style.height = (window.outerHeight + 50) + 'px';
-        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+        setTimeout(function () { window.scrollTo(0, 1); }, 50);
     }
-    else
-    {
-        setTimeout( function(){ window.scrollTo(0, 1); }, 0 ); 
+    else {
+        setTimeout(function () { window.scrollTo(0, 1); }, 0);
     }
 }
 );
--- routes/content.js
+++ routes/content.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Browser listing. */
-router.get('/browser', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('browser',  { title: 'Browser' });
+router.get('/browser', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('browser', { title: 'Browser' });
 });
 
 /* GET Kontakt listing. */
-router.get('/kontakt', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('kontakt',  { title: 'Kontakt anlegen' });
+router.get('/kontakt', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('kontakt', { title: 'Kontakt anlegen' });
 });
 
 /* GET Mausklick listing. */
-router.get('/mausklick', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('mausklick',  { title: 'Mausklick' });
+router.get('/mausklick', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('mausklick', { title: 'Mausklick' });
 });
 
 /* GET Shortcuts listing. */
-router.get('/shortcuts', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('shortcuts',  { title: 'Shortcuts' });
+router.get('/shortcuts', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('shortcuts', { title: 'Shortcuts' });
 });
 
 /* GET WhatsApp listing. */
-router.get('/whatsapp', function(req, res, next) {   //hier blos nix reinschreiben nach dem /'
-  res.render('whatsapp',  { title: 'WhatsApp' });
+router.get('/whatsapp', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('whatsapp', { title: 'WhatsApp' });
 });
 
 module.exports = router;

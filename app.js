60efad7ba75e9feaec7f3bc9b23ac51cdf9e0782
mikioosaki <mikio.osaki@posteo.de>
2019-03-30 17:52:28 +0100

    stash test

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/wunschbeitrag', wunschbeitragRouter);
+app.use('/trello', wunschbeitragRouter);
 app.use('/content', contentRouter);
 
 // catch 404 and forward to error handler

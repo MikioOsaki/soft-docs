65b3db7b0ef99031f5e0585ccb8fb2d8b7c27f75
mikioosaki <mikio.osaki@posteo.de>
2019-03-30 17:27:29 +0100

    reactivate deleted routes

--- routes/content.js
+++ routes/content.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET Browser listing. */
+router.get('/browser', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('browser', { title: 'Browser' });
+});
 
+/* GET Kontakt listing. */
+router.get('/kontakt', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('kontakt', { title: 'Kontakt anlegen' });
+});
+
+/* GET Mausklick listing. */
+router.get('/mausklick', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('mausklick', { title: 'Mausklick' });
+});
+
+/* GET Shortcuts listing. */
+router.get('/shortcuts', function (req, res, next) {   //hier blos nix reinschreiben nach dem /'
+  res.render('shortcuts', { title: 'Shortcuts' });
+});
+
+/* GET WhatsApp listing. */
+router.get('/whatsapp', function (req, res, next) {
+  res.render('whatsapp', { title: 'WhatsApp' });
+});
 
 /* GET Bitwarden listing. */
 router.get('/bitwarden', function (req, res, next) {
